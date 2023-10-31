e73c237465051a550776eb324cb25bd9d7518111
Daruich, Nicolas Alberto <nic.aldar@hotmail.com>
2023-10-31 19:09:09 -0300

    Playground intermedio Parte 1: completado

--- main/views.py
+++ main/views.py
@@ -3,11 +3,11 @@ from main.models import Patients
 
 # Create your views here:
 def main(request):
-    return render(request, 'main.html', {})
+    return render(request, 'main/main.html', {})
 
 def patients(request):
     
     patient = Patients(name='Blue', lastname='Label', height=1.8, weight=80, bloodGlucose=90, triglycerides=120)
     patient.save()
 
-    return render(request, 'results.html', {'patients': patient})
+    return render(request, 'main/results.html', {'patients': patient})

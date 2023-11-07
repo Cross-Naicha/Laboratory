9195a2181a22356d0786c33a2a3e2fdbeb9f37e6
Daruich, Nicolas Alberto <nic.aldar@hotmail.com>
2023-11-07 17:55:44 -0300

    Login completado

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from main.views import main, login
+from main.views import main, login, register
+from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
-    path('', login, name='accounts_path'),
+    path('', login, name='accounts_login_path'),
+    path('accounts/register/', register, name='accounts_register_path'),
+    path('accounts/logout/', LogoutView.as_view(template_name='main/logout.html'), name='accounts_logout_path'),
     path('index/', main, name='index_path'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login as auth_login
 
 # Create your views here:
 def main(request):
     return render(request, 'main/index.html', {})
 
 def login(request):
-    return render(request, 'main/login.html', {})
+    
+    authentication_form = AuthenticationForm()
+
+    if request.method == 'POST':
+        authentication_form = AuthenticationForm(request, data=request.POST)
+        if authentication_form.is_valid():
+            user = authentication_form.cleaned_data.get('username')
+            password = authentication_form.cleaned_data.get('password')
+
+            active_user = authenticate(username=user, password=password)
+
+            auth_login(request, active_user)
+
+            return redirect('index_path')
+        else:
+            return render(request, 'main/login-denied.html', {})
+
+    return render(request, 'main/login.html', {'authentication_form': authentication_form})
+
+def register(request):
+    return render(request, 'main/register.html', {})
+    
+
